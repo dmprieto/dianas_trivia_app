9cb4601e3b36128ca18fb4995bac2de740dc5c0e
dmprieto <dianam.prieto@gmail.com>
2023-02-06 16:46:59 -0500

    #Added Nativewind dependency
    -yarn add nativewind
    -yarn add --dev tailwindcss
    -Run npx tailwindcss init to create a tailwind.config.js file
    -modify tailwind.config.js to include proper paths
    -update babel

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/App.{js,jsx,ts,tsx}", 
+    "./app/screens/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
