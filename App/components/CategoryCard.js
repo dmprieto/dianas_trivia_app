51ee6ae99facbeef253344dd9426c3c386e85eb9
dmprieto <dianam.prieto@gmail.com>
2023-02-08 11:00:39 -0500

    category card component and app background

---
+++ App/components/CategoryCard.js
@@ -0,0 +1,10 @@
+import { Text, TouchableOpacity } from "react-native";
+
+export const CategoryCard = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className="py-5 px-3 opacity-90 bg-purple-900 text-white text-lg font-semibold rounded-md mt-4"
+  >
+    <Text className="text-2xl text-purple-100">{title}</Text>
+  </TouchableOpacity>
+);
