c4a59ce6fbb0e16c391547bf9b926ccd80e679dc
dmprieto <dianam.prieto@gmail.com>
2023-02-13 12:44:53 -0500

    show score and clean code

--- App/components/quiz/EndQuiz.js
+++ App/components/quiz/EndQuiz.js
@@ -1,27 +1,61 @@
 import LottieView from "lottie-react-native"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { FlatList, Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { DetailedQuestion } from "./DetailedQuestion"
 
+const calculateScore = (questions, answers) => {
+  let tempScore = 0
+  console.log("started rfect")
+  questions.forEach((question, index) => {
+    const tempAnswer = answers[index].answer
+    if (tempAnswer && question.correct_answer === tempAnswer) {
+      tempScore++
+    }
+  })
+
+  console.log("finalscore:" + tempScore)
+  return tempScore
+}
+
 export const EndQuiz = ({ questions, category, quizStatus, answers }) => {
   if (quizStatus && quizStatus === "ended") {
+    const [showResults, setShowResults] = useState(false)
+    const [score, setScore] = useState(0)
     const animation = useRef(null)
+
+    useEffect(() => {
+      setScore(calculateScore(questions, answers))
+      const timer = setTimeout(() => {
+        setShowResults(true)
+      }, 3000)
+      return () => clearTimeout(timer)
+    }, [])
+
+    if (!showResults) {
+      return (
+        <SafeAreaView className="flex-1 items-center justify-center">
+          <LottieView
+            autoPlay
+            loop={false}
+            ref={animation}
+            style={{
+              width: 200,
+              height: 200,
+              backgroundColor: "transparent"
+            }}
+            // Find more Lottie files at https://lottiefiles.com/featured
+            source={require("../../assets/game-complete.json")}
+          />
+          <Text className="text-lg text-fuchsia-200 font-bold text-center tracking-wide pt-10">{`Calculating Results ...`}</Text>
+        </SafeAreaView>
+      )
+    }
     return (
       <SafeAreaView className="flex-1 items-center justify-center">
-        <Text className="text-lg text-fuchsia-200 font-bold text-center tracking-wide pt-10">{`${category} Quiz Results`}</Text>
-        <LottieView
-          autoPlay
-          loop={false}
-          ref={animation}
-          style={{
-            width: 200,
-            height: 200,
-            backgroundColor: "transparent"
-          }}
-          // Find more Lottie files at https://lottiefiles.com/featured
-          source={require("../../assets/game-complete.json")}
-        />
+        <Text className="text-2xl text-fuchsia-200 font-semibold text-center tracking-wide pt-10">{`${category} Quiz Results`}</Text>
+        <Text className="text-xl text-white font-bold text-center tracking-wide">{`Score: ${score}/10`}</Text>
+
         <FlatList
           className="w-screen"
           data={answers}
--- App/components/quiz/Question.js
+++ App/components/quiz/Question.js
@@ -19,87 +19,6 @@ const setNextStep = (
   })
 }
 
-const setFinalStep = (setQuizStatus) => {
-  setQuizStatus("ended")
-}
-
-const getLeftArrowComponent = (
-  currentQuestion,
-  setCurrentQuestion,
-  questions,
-  answers,
-  setAnswer
-) => {
-  return (
-    <TouchableOpacity
-      onPress={() => {
-        setAnswer(answers[currentQuestion.id - 1].answer)
-        setNextStep(currentQuestion, setCurrentQuestion, questions, false)
-      }}
-      disabled={currentQuestion.id === 0}
-    >
-      <MaterialCommunityIcons
-        size={90}
-        color={currentQuestion.id === 0 ? "transparent" : "#d4d4d8"}
-        name="menu-left"
-      />
-    </TouchableOpacity>
-  )
-}
-
-const getRightArrowComponent = (
-  currentQuestion,
-  setCurrentQuestion,
-  questions,
-  answer,
-  setAnswer,
-  answers,
-  setAnswers,
-  setQuizStatus
-) => {
-  const color =
-    currentQuestion.id === questions.length - 1 ? "#06b6d4" : "#d4d4d8"
-  // if it reached the last question, end quiz
-  return (
-    <TouchableOpacity
-      onPress={() => {
-        const nextAnswer = answers.find((answer) => {
-          return answer.id === currentQuestion.id + 1
-        })
-        if (nextAnswer !== undefined) {
-          setAnswer(nextAnswer.answer)
-        } else {
-          setAnswer("")
-        }
-
-        const currentAnswer = answers.find((answer) => {
-          return answer.id === currentQuestion.id
-        })
-
-        if (currentAnswer !== undefined) {
-          currentAnswer.answer = answer
-          setAnswers([...answers])
-        } else {
-          setAnswers([...answers, { id: currentQuestion.id, answer: answer }])
-        }
-
-        if (currentQuestion.id === questions.length - 1) {
-          setFinalStep(setQuizStatus)
-        } else {
-          setNextStep(currentQuestion, setCurrentQuestion, questions, true)
-        }
-      }}
-      disabled={answer === ""}
-    >
-      <MaterialCommunityIcons
-        size={90}
-        color={answer === "" ? "#6b7280" : color}
-        name="menu-right"
-      />
-    </TouchableOpacity>
-  )
-}
-
 export const Question = ({
   currentQuestion,
   setCurrentQuestion,
@@ -110,7 +29,27 @@ export const Question = ({
   setAnswers
 }) => {
   const [answer, setAnswer] = useState("")
+  function evalNextAnswer() {
+    const nextAnswer = answers.find((answer) => {
+      return answer.id === currentQuestion.id + 1
+    })
+    nextAnswer !== undefined ? setAnswer(nextAnswer.answer) : setAnswer("")
+  }
+  function evalCurrentAnswer() {
+    const currentAnswer = answers.find((answer) => {
+      return answer.id === currentQuestion.id
+    })
+
+    if (currentAnswer !== undefined) {
+      currentAnswer.answer = answer
+      setAnswers([...answers])
+    } else {
+      setAnswers([...answers, { id: currentQuestion.id, answer: answer }])
+    }
+  }
   if (quizStatus && quizStatus === "started") {
+    const color =
+      currentQuestion.id === questions.length - 1 ? "#06b6d4" : "#d4d4d8"
     return (
       <SafeAreaView className="flex-1 py-5">
         <Text className="text-xl text-fuchsia-200 font-bold text-center">{`${currentQuestion.category} Quiz`}</Text>
@@ -119,29 +58,49 @@ export const Question = ({
         } Of ${questions.length}`}</Text>
         <Text className="text-xl text-slate-50 font-bold text-center px-10 pt-5">{`${currentQuestion.question}?`}</Text>
         <View className="flex-1 flex-row space-x-1">
-          {getLeftArrowComponent(
-            currentQuestion,
-            setCurrentQuestion,
-            questions,
-            answers,
-            setAnswer
-          )}
+          <TouchableOpacity
+            onPress={() => {
+              setAnswer(answers[currentQuestion.id - 1].answer)
+              setNextStep(currentQuestion, setCurrentQuestion, questions, false)
+            }}
+            disabled={currentQuestion.id === 0}
+          >
+            <MaterialCommunityIcons
+              size={90}
+              color={currentQuestion.id === 0 ? "transparent" : "#d4d4d8"}
+              name="menu-left"
+            />
+          </TouchableOpacity>
 
           <MultipleOptions
             options={currentQuestion.allOptions}
             answer={answer}
             setAnswer={setAnswer}
           />
-          {getRightArrowComponent(
-            currentQuestion,
-            setCurrentQuestion,
-            questions,
-            answer,
-            setAnswer,
-            answers,
-            setAnswers,
-            setQuizStatus
-          )}
+          <TouchableOpacity
+            onPress={() => {
+              evalNextAnswer()
+              evalCurrentAnswer()
+
+              if (currentQuestion.id === questions.length - 1) {
+                setQuizStatus("ended")
+              } else {
+                setNextStep(
+                  currentQuestion,
+                  setCurrentQuestion,
+                  questions,
+                  true
+                )
+              }
+            }}
+            disabled={answer === ""}
+          >
+            <MaterialCommunityIcons
+              size={90}
+              color={answer === "" ? "#6b7280" : color}
+              name="menu-right"
+            />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     )
