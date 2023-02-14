053eeb37900b74fa1003ee2ff39df3f09739eda3
dmprieto <dianam.prieto@gmail.com>
2023-02-14 17:50:16 -0500

    welcome animated svg

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,32 +1,29 @@
 import LottieView from "lottie-react-native"
 import { useRef } from "react"
-import { ImageBackground, Text, View } from "react-native"
+import { ImageBackground, View } from "react-native"
 import { AppButton } from "../components/AppButton"
+import WelcomeTrivia from "../components/WelcomeTrivia"
 
 const bgImage = "../assets/background/init.jpg"
 
 export default ({ navigation }) => {
   const animation = useRef(null)
   return (
-    <ImageBackground
-      className="flex-1 items-center justify-center"
-      source={require(bgImage)}
-    >
+    <ImageBackground className="flex-1" source={require(bgImage)}>
       <View className="items-center justify-center absolute inset-5 backdrop-blur bg-black/30">
-        <Text className="text-4xl text-fuchsia-200 font-bold text-center tracking-wide">
-          Welcome to Diana's Trivia!!!!
-        </Text>
         <LottieView
           autoPlay
           ref={animation}
           style={{
-            width: 200,
-            height: 200,
+            width: 150,
+            height: 150,
             backgroundColor: "transparent"
           }}
           // Find more Lottie files at https://lottiefiles.com/featured
           source={require("../assets/game-start.json")}
         />
+        <WelcomeTrivia />
+
         <AppButton
           title="Let's Play"
           onPress={() => {
